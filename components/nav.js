ca9cfaf05d7e65f429af3756647a31e36d591b1c
Janberka <janberka@gmail.com>
2019-09-21 00:38:01 +0300

    branding
    :)

--- components/nav.js
+++ components/nav.js
@@ -2,8 +2,8 @@ import React from 'react'
 import Link from 'next/link'
 
 const links = [
-  { href: 'https://zeit.co/now', label: 'ZEIT' },
-  { href: 'https://github.com/zeit/next.js', label: 'GitHub' }
+  { href: 'https://janberkarikan.com', label: 'Janberk Arıkan' },
+  { href: 'https://github.com/Janberka/next-test', label: 'GitHub' }
 ].map(link => {
   link.key = `nav-link-${link.href}-${link.label}`
   return link
@@ -14,7 +14,7 @@ const Nav = () => (
     <ul>
       <li>
         <Link href='/'>
-          <a>Janberk Arıkan</a>
+          <a>Home</a>
         </Link>
       </li>
       {links.map(({ key, href, label }) => (
